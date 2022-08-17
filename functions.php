c6fadfc323576ed184bef23011cd5925434cd71a
Paweł Dziedzic <pdziedzic@gravityglobal.com>
2022-08-17 10:58:26 +0200

    code refactored

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Scope of the application:
  * 1. Allow guests to login to their wordpress account.
@@ -12,109 +14,108 @@
  * 2. Code style.
  * 3. Potential performance issues.
  */
+\add_filter('show_admin_bar', '__return_false');
 
-add_filter( 'show_admin_bar', '__return_false' );
-
-add_action('wp_ajax_nopriv_load_view', 'load_view');
-add_action('wp_ajax_load_view', 'load_view');
+\add_action('wp_ajax_nopriv_load_view', 'load_view');
+\add_action('wp_ajax_load_view', 'load_view');
 
 function load_view(): void
 {
-    require 'partials/' . $_GET['view'] . '.php';
-    die;
+    $view = $_GET['view'] ?: false;
+
+    if (false === $view) {
+        return;
+    }
+
+    require_once "partials/{$view}.php";
+    exit;
 }
 
-add_action('wp_ajax_nopriv_login', 'login');
-add_action('wp_ajax_login', 'login');
+\add_action('wp_ajax_nopriv_login', 'login');
 
 function login(): void
 {
-    $email = $_GET['email'];
-    $password = $_GET['password'];
+    $email = \filter_input(\INPUT_GET, 'email', \FILTER_SANITIZE_EMAIL);
+    $password = \filter_input(\INPUT_GET, 'password', \FILTER_SANITIZE_STRING);
 
-    if ($email) {
-        if ($password) {
-            $user = wp_signon(['user_login' => $email, 'user_password' => $password]);
-            if (is_wp_error($user)) {
-                echo $user->get_error_message();
-                http_response_code(400);
-                die;
-            }
-        }else {
-            echo 'Missing password!';
-            http_response_code(400);
-            die;
-        }
-    } else {
+    if (empty($email) || empty($password)) {
         echo 'Missing email!';
-        http_response_code(400);
-        die;
+        \http_response_code(400);
+        exit;
     }
 
-    http_response_code(200);
-    die;
+    $user = \wp_signon(['user_login' => $email, 'user_password' => $password]);
+    if (\is_wp_error($user)) {
+        echo $user->get_error_message();
+        \http_response_code(400);
+        exit;
+    }
+
+    \http_response_code(200);
+    exit;
 }
 
-add_action('wp_ajax_nopriv_post_delete', 'post_delete');
-add_action('wp_ajax_post_delete', 'post_delete');
+\add_action('wp_ajax_post_delete', 'post_delete');
 
 function post_delete(): void
 {
-    $post_id = $_GET['post'];
+    $post_id = \filter_input(\INPUT_GET, 'post', \FILTER_SANITIZE_NUMBER_INT);
 
-    if ($post_id) {
-        $delete = wp_delete_post($post_id);
-
-            if (!$delete) {
-                echo 'Post cannot be delted';
-                http_response_code(400);
-                die;
-            }
-
-    } else {
+    if (empty($post_id)) {
         echo 'Invalid post id!';
-        http_response_code(400);
-        die;
+        \http_response_code(400);
+        exit;
     }
 
-    http_response_code(200);
-    die;
+    $deleted = \wp_delete_post($post_id);
+
+    if (false === $deleted) {
+        echo 'Post cannot be delted';
+        \http_response_code(400);
+        exit;
+    }
+
+    \http_response_code(200);
+    exit;
 }
 
-add_action('wp_ajax_nopriv_post_insert', 'post_insert');
-add_action('wp_ajax_post_insert', 'post_insert');
+\add_action('wp_ajax_post_insert', 'post_insert');
 
 function post_insert(): void
 {
-    $post = json_decode(stripslashes($_GET['post']), true);
-    $post = filter_var_array($post);
+    $post = \json_decode(\stripslashes($_GET['post'] ?? null), true);
+    $post = \filter_var_array(
+        $post,
+        [
+            'id' => \FILTER_SANITIZE_NUMBER_INT,
+            'post_content' => \FILTER_SANITIZE_STRING,
+        ]
+    );
 
-    if ($post) {
-        $post = wp_insert_post($post);
-
-        if (is_wp_error($post)) {
-            echo $post->get_error_message();
-            http_response_code(400);
-            die;
-        }
-
-    } else {
+    if (empty($post)) {
         echo 'Invalid post!';
-        http_response_code(400);
-        die;
+        \http_response_code(400);
+        exit;
     }
 
-    http_response_code(200);
-    die;
+    $post = \wp_insert_post($post);
+
+    if (\is_wp_error($post)) {
+        echo $post->get_error_message();
+        \http_response_code(400);
+        exit;
+    }
+
+    \http_response_code(200);
+    exit;
 }
 
-add_action('wp_ajax_nopriv_logout', 'logout');
-add_action('wp_ajax_logout', 'logout');
+\add_action('wp_ajax_logout', 'logout');
 
 function logout(): void
 {
-    wp_logout();
+    \wp_logout();
 
-    http_response_code(200);
-    die;
+    \http_response_code(200);
+    exit;
 }
